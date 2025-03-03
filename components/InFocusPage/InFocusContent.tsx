c567fe1c749e56fc904818f33310f679dd2e7bf6
Armin Babic <crvenioklop@gmail.com>
2025-03-03 10:04:39 +0100

    fix bug with wihite space on bottom on in focus single page

--- components/InFocusPage/InFocusContent.tsx
+++ components/InFocusPage/InFocusContent.tsx
@@ -27,7 +27,9 @@ const InFocusContent = ({ infocus }: any) => {
             {infocus.attributes.contentB}
           </p>
 
-          <p className="pb-6">{infocus.attributes.contentC}</p>
+          <p className={infocus.attributes.contentC ? "pb-6" : "hidden"}>
+            {infocus.attributes.contentC}{" "}
+          </p>
 
           {/*   <div className="relative w-full h-[24rem] z-0 object-cover mb-6 ">
             <Image
@@ -38,54 +40,144 @@ const InFocusContent = ({ infocus }: any) => {
             />
           </div> */}
 
-          <p className="pb-6">{infocus.attributes.contentD}</p>
+          <p className={infocus.attributes.contentD ? "pb-6" : "hidden"}>
+            {infocus.attributes.contentD}
+          </p>
 
-          <p className="pb-6">{infocus.attributes.contentE}</p>
-          <p className="pb-6">{infocus.attributes.contentF}</p>
+          <p className={infocus.attributes.contentE ? "pb-6" : "hidden"}>
+            {infocus.attributes.contentE}
+          </p>
+          <p className={infocus.attributes.contentF ? "pb-6" : "hidden"}>
+            {infocus.attributes.contentF}
+          </p>
 
-          <p className="pb-6">{infocus.attributes.contentG}</p>
-          <p className="pb-6">{infocus.attributes.contentH}</p>
-          <p className="pb-6">{infocus.attributes.contentI}</p>
-          <p className="pb-6">{infocus.attributes.contentJ}</p>
-          <p className=" border-l-4 border-gray-500 pl-4 mb-6 italic rounded ">
+          <p className={infocus.attributes.contentG ? "pb-6" : "hidden"}>
+            {infocus.attributes.contentG}
+          </p>
+          <p className={infocus.attributes.contentH ? "pb-6" : "hidden"}>
+            {infocus.attributes.contentH}
+          </p>
+          <p className={infocus.attributes.contentI ? "pb-6" : "hidden"}>
+            {infocus.attributes.contentI}
+          </p>
+          <p className={infocus.attributes.contentJ ? "pb-6" : "hidden"}>
+            {infocus.attributes.contentJ}
+          </p>
+          <p
+            className={
+              infocus.attributes.contentK
+                ? "border-l-4 border-gray-500 pl-4 mb-6 italic rounded"
+                : "hidden"
+            }
+          >
             {infocus.attributes.contentK}
           </p>
-          <p className="pb-6">{infocus.attributes.contentL}</p>
-          <p className="pb-6">{infocus.attributes.contentM}</p>
-          <p className="pb-6">{infocus.attributes.contentN}</p>
-          <p className="pb-6">{infocus.attributes.contentO}</p>
-          <p className="pb-6">{infocus.attributes.contentP}</p>
-          <p className="pb-6">{infocus.attributes.contentR}</p>
-          <p className="pb-6">{infocus.attributes.contentS}</p>
-          <p className=" border-l-4 border-gray-500 pl-4 mb-6 italic rounded">
+          <p className={infocus.attributes.contentL ? "pb-6" : "hidden"}>
+            {infocus.attributes.contentL}
+          </p>
+          <p className={infocus.attributes.contentM ? "pb-6" : "hidden"}>
+            {infocus.attributes.contentM}
+          </p>
+          <p className={infocus.attributes.contentN ? "pb-6" : "hidden"}>
+            {infocus.attributes.contentN}
+          </p>
+          <p className={infocus.attributes.contentO ? "pb-6" : "hidden"}>
+            {infocus.attributes.contentO}
+          </p>
+          <p className={infocus.attributes.contentP ? "pb-6" : "hidden"}>
+            {infocus.attributes.contentP}
+          </p>
+          <p className={infocus.attributes.contentR ? "pb-6" : "hidden"}>
+            {infocus.attributes.contentR}
+          </p>
+          <p className={infocus.attributes.contentS ? "pb-6" : "hidden"}>
+            {infocus.attributes.contentS}
+          </p>
+          <p
+            className={
+              infocus.attributes.contentT
+                ? "border-l-4 border-gray-500 pl-4 mb-6 italic rounded"
+                : "hidden"
+            }
+          >
             {infocus.attributes.contentT}
           </p>
-          <p className="pb-6">{infocus.attributes.contentU}</p>
-          <p className="pb-6">{infocus.attributes.contentV}</p>
-          <p className="pb-6">{infocus.attributes.contentZ}</p>
-          <p className="pb-6">{infocus.attributes.conclusion}</p>
-          <p className="pb-6">{infocus.attributes.textA}</p>
-          <p className="pb-6">{infocus.attributes.textB}</p>
-          <p className="pb-6">{infocus.attributes.textC}</p>
-          <p className="pb-6">{infocus.attributes.textD}</p>
-          <p className="border-l-4 border-gray-500 pl-4 mb-6 italic rounded">
+          <p className={infocus.attributes.contentU ? "pb-6" : "hidden"}>
+            {infocus.attributes.contentU}
+          </p>
+          <p className={infocus.attributes.contentV ? "pb-6" : "hidden"}>
+            {infocus.attributes.contentV}
+          </p>
+          <p className={infocus.attributes.contentZ ? "pb-6" : "hidden"}>
+            {infocus.attributes.contentZ}
+          </p>
+          <p className={infocus.attributes.conclusion ? "pb-6" : "hidden"}>
+            {infocus.attributes.conclusion}
+          </p>
+          <p className={infocus.attributes.textA ? "pb-6" : "hidden"}>
+            {infocus.attributes.textA}
+          </p>
+          <p className={infocus.attributes.textB ? "pb-6" : "hidden"}>
+            {infocus.attributes.textB}
+          </p>
+          <p className={infocus.attributes.textC ? "pb-6" : "hidden"}>
+            {infocus.attributes.textC}
+          </p>
+          <p className={infocus.attributes.textD ? "pb-6" : "hidden"}>
+            {infocus.attributes.textD}
+          </p>
+          <p
+            className={
+              infocus.attributes.textE
+                ? "border-l-4 border-gray-500 pl-4 mb-6 italic rounded"
+                : "hidden"
+            }
+          >
             {infocus.attributes.textE}
           </p>
-          <p className="pb-6">{infocus.attributes.textF}</p>
-          <p className="pb-6">{infocus.attributes.textG}</p>
-          <p className="pb-6">{infocus.attributes.textH}</p>
-          <p className="border-l-4 border-gray-500 pl-4 mb-6 italic rounded">
+          <p className={infocus.attributes.textF ? "pb-6" : "hidden"}>
+            {infocus.attributes.textF}
+          </p>
+          <p className={infocus.attributes.textG ? "pb-6" : "hidden"}>
+            {infocus.attributes.textG}
+          </p>
+          <p className={infocus.attributes.textH ? "pb-6" : "hidden"}>
+            {infocus.attributes.textH}
+          </p>
+          <p
+            className={
+              infocus.attributes.textI
+                ? "border-l-4 border-gray-500 pl-4 mb-6 italic rounded"
+                : "hidden"
+            }
+          >
             {infocus.attributes.textI}
           </p>
-          <p className="pb-6">{infocus.attributes.textJ}</p>
-          <p className="pb-6">{infocus.attributes.textK}</p>
-          <p className="pb-6">{infocus.attributes.textL}</p>
+          <p className={infocus.attributes.textJ ? "pb-6" : "hidden"}>
+            {infocus.attributes.textJ}
+          </p>
+          <p className={infocus.attributes.textK ? "pb-6" : "hidden"}>
+            {infocus.attributes.textK}
+          </p>
+          <p className={infocus.attributes.textL ? "pb-6" : "hidden"}>
+            {infocus.attributes.textL}
+          </p>
 
-          <p className="border-l-4 border-gray-500 pl-4 mb-6 italic rounded">
+          <p
+            className={
+              infocus.attributes.textM
+                ? "border-l-4 border-gray-500 pl-4 mb-6 italic rounded"
+                : "hidden"
+            }
+          >
             {infocus.attributes.textM}
           </p>
-          <p className="pb-6">{infocus.attributes.textN}</p>
-          <p className="pb-6">{infocus.attributes.textO}</p>
+          <p className={infocus.attributes.textN ? "pb-6" : "hidden"}>
+            {infocus.attributes.textN}
+          </p>
+          <p className={infocus.attributes.textO ? "pb-6" : "hidden"}>
+            {infocus.attributes.textO}
+          </p>
         </div>
       </main>
     </div>
