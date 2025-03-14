1bfcc5b85b949628a3e04aafbf7cd7f436dd568d
Armin Babic <crvenioklop@gmail.com>
2025-03-14 09:08:25 +0100

    change about us part image

--- components/AboutUs.js
+++ components/AboutUs.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Image from "next/image";
-import coreImg from "../public/assets/aboutus.jpg";
+import coreImg from "../public/assets/team2.jpg";
 import Link from "next/link";
 
 const AboutUs = () => {
