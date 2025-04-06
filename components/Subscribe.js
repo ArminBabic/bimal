68fb3851191b2efcdb2fb42d99519d3e4c0a90df
ArminBabic <crvenioklop@gmail.com>
2025-04-06 23:40:55 +0200

    fix subscribe form

--- components/Subscribe.js
+++ components/Subscribe.js
@@ -1,64 +1,62 @@
-import React from "react";
-import { BiSupport } from "react-icons/bi";
-import { TbMessages, TbBulb } from "react-icons/tb";
-import { GiCheckMark } from "react-icons/gi";
-import { BsPeopleFill } from "react-icons/bs";
+import React, { useState, useRef } from "react";
+import emailjs from "@emailjs/browser";
+import toast, { Toaster } from "react-hot-toast";
+
+const notify = () => {
+  toast.error("Please make sure to enter your email!");
+};
+
+const notifyNetwork = () => {
+  toast.error("Something went wrong with email services!");
+};
+
+const notifySuccess = () => {
+  toast.success("Message was sent!");
+};
 
 const Subscribe = () => {
+  const form = useRef();
+  const [email, setEmail] = useState("");
+  const [buttonText, setButtonText] = useState("Subscribe");
+
+  // Regex for validating email
+  const formValidty = email.includes("@") && /\S+@\S+\.\S+/.test(email);
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+    
+    // If email is valid
+    if (formValidty) {
+      setButtonText("Sending...");
+
+      emailjs
+        .sendForm(
+          "service_45fgwdv",
+          "template_tqb5mqv",
+          form.current,
+          "1JJMrN8wOdT6wa--I"
+        )
+        .then(
+          (result) => {
+            e.target.reset();
+            setEmail("");
+            notifySuccess();
+            setButtonText("Subscribe");
+          },
+          (error) => {
+            console.log(error.text);
+            notifyNetwork();
+            setButtonText("Subscribe");
+          }
+        );
+    } else {
+      notify();
+    }
+  };
+
   return (
-    <div
-      id="subscribe"
-      className="w-full flex flex-col items-center my-16 px-3 gap-16"
-    >
-      {" "}
-      {/*  <section className="w-full max-w-[1420px] pt-1  px-2 xs:px-6 sm:px-16 xl:px-2 2xl:px-16  gap-6 xl:gap-10 2xl:gap-48 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3  place-items-center ">
-        <article
-          className="max-w-[250px] xs:w-[380px] sm:w-[256px] md:w-[310px]
-            xl:w-[250px] h-[256px] 2xl:w-[17.5rem]   flex flex-col gap-4 justify-center items-center text-center 
-            cursor-pointer "
-        >
-          <div className=" relative flex items-center justify-center rounded-3xl w-16 h-16">
-            <BsPeopleFill className=" text-secondary w-[3.5rem] h-[3.5rem] relative" />
-          </div>
-          <h4 className="font-bold text-[#303030]  text-lg">
-            Join the Network
-          </h4>
-          <p className="text-base text-[#303030]  ">
-            Subscribe to our blog to receive expert tips, advice, and insights
-            from our experts.
-          </p>
-        </article>
-        <article
-          className="max-w-[250px] xs:w-[380px] sm:w-[256px] md:w-[310px]
-            xl:w-[250px] h-[256px] 2xl:w-[17.5rem]   flex flex-col gap-4 justify-center items-center text-center 
-            cursor-pointer "
-        >
-          <div className=" relative flex items-center justify-center rounded-3xl w-16 h-16">
-            <TbMessages className=" text-secondary w-[3.5rem] h-[3.5rem] relative" />
-          </div>
-          <h4 className="font-bold text-[#303030]  text-lg">
-            Digital Conversation
-          </h4>
-          <p className="text-base text-[#303030]  ">
-            Reach a broader audience, expand their brand presence, and gather
-            valuable feedback.
-          </p>
-        </article>{" "}
-        <article
-          className="max-w-[250px] xs:w-[380px] sm:w-[256px] md:w-[310px]
-            xl:w-[250px] h-[256px] 2xl:w-[17.5rem]   flex flex-col gap-4 justify-center items-center text-center 
-            cursor-pointer "
-        >
-          <div className=" relative flex items-center justify-center rounded-3xl w-16 h-16">
-            <TbBulb className=" text-secondary w-[3.5rem] h-[3.5rem] relative" />
-          </div>
-          <h4 className="font-bold text-[#303030]  text-lg">Great Ideas</h4>
-          <p className="text-base text-[#303030]  ">
-            We foster a culture of creativity and encourage employees to think
-            outside the box.
-          </p>
-        </article>
-      </section> */}
+    <div id="subscribe" className="w-full flex flex-col items-center my-16 px-3 gap-16">
+      <Toaster position="top-center" reverseOrder={true} />
       <section className="bg-secondary w-full max-w-[1380px] flex flex-col gap-6 items-center rounded-xl py-12 sm:py-20 px-4">
         <h4 className=" text-2xl sm:text-4xl font-semibold  text-white text-center max-w-[540px]">
           Join our community
@@ -68,28 +66,34 @@ const Subscribe = () => {
           by subscribing to our blog
         </p>
 
-        {/* mobile form */}
-        <form className="mt-8 flex flex-col gap-2 w-full sm:hidden max-w-[400px]">
+        {/* Mobile form */}
+        <form ref={form} onSubmit={sendEmail} className="mt-8 flex flex-col gap-2 w-full sm:hidden max-w-[400px]">
           <input
             type="email"
+            htmlFor="email"
+            name="user_email"
             placeholder="Enter email address"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
             className="rounded-[3rem] border-4 border-white pl-6 pr-6 py-4 outline-none w-full placeholder:text-secondary placeholder:text-center"
           />
-
-          <button className="text-white bg-secondary border-white  w-full rounded-[30px] border-4 py-4 hover:bg-white hover:text-secondary border-secondary ease-in duration-300">
-            Subscribe
+          <button type="submit" className="text-white bg-secondary border-white w-full rounded-[30px] border-4 py-4 hover:bg-white hover:text-secondary border-secondary ease-in duration-300">
+            {buttonText}
           </button>
         </form>
 
-        {/* desktop form */}
-        <form className="bg-white rounded-[3rem] py-1 items-center gap-20 mt-12 hidden sm:flex">
+        {/* Desktop form */}
+        <form ref={form} onSubmit={sendEmail} className="bg-white rounded-[3rem] py-1 items-center gap-20 mt-12 hidden sm:flex w-full max-w-[600px]">
           <input
             type="email"
+            name="user_email"
             placeholder="Enter email address"
-            className="rounded-[3rem] pl-6 pr-20 py-5 outline-none placeholder:text-secondary"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            className="rounded-[3rem] pl-6 pr-20 py-5 outline-none placeholder:text-secondary w-full"
           />
-          <button className="text-white bg-secondary rounded-[30px] border-2 py-2 sm:py-4 px-8 mx-1 hover:bg-white hover:text-secondary border-secondary ease-in duration-300">
-            Subscribe
+          <button type="submit" className="text-white bg-secondary rounded-[30px] border-2 py-2 sm:py-4 px-8 mx-1 hover:bg-white hover:text-secondary border-secondary ease-in duration-300">
+            {buttonText}
           </button>
         </form>
       </section>
